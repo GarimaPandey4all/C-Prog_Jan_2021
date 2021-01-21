3c98e81013b82d6d3783325b76e1cf496cb6bd11
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-01-21 22:05:57 +0530

    Add files via upload

---
+++ HELLORUS.C
@@ -0,0 +1,23 @@
+/*Program: Hello World*/  //Single Line Comment
+/*Multiple Line Comment*/
+
+/*Pre - Processor Directives*/
+#include<stdio.h>
+#include<conio.h>
+
+/*
+	# - Pre-Processor
+	include - directive/directory/folder
+	stdio.h- header file
+
+	stdio.h - standard input/output
+
+	conio.h - console/output screen/Terminal input / output
+*/
+
+void main()  // void - empty or null , main()-Entry Point
+{
+	clrscr(); //clear screen
+	printf("Hello World"); // print the output on output screen
+	getch();  // get character
+}
